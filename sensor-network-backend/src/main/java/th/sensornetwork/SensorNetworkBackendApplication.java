da8568314b9d7ce86b5a0046a6e97f5d1083f9cd
artem <polevoi.92@gmail.com>
2018-07-20 11:06:37 +0200

    initial commit

---
+++ sensor-network-backend/src/main/java/th/sensornetwork/SensorNetworkBackendApplication.java
@@ -0,0 +1,12 @@
+package th.sensornetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorNetworkBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SensorNetworkBackendApplication.class, args);
+    }
+}
